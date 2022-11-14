e236614e47c7ab67bacf73304dc779d174df905c
Galligani Joey <97046523+Joey-Galligani@users.noreply.github.com>
2022-11-14 22:54:59 +0100

    Add files via upload

--- TP1-R309/TP1-R309-ex4.py
+++ TP1-R309/TP1-R309-ex4.py
@@ -20,10 +20,23 @@ def carre_noir(x, y):
 		i += 1
 		x += taille_carre * 2
 
+def pion():
+	"Dessine un pion au hazard sur le damier"
+	x = taille_carre/2 + randrange(8) * taille_carre
+	y = taille_carre/2 + randrange(8) * taille_carre
+	creation_pion(x, y, taille_carre/3, "blue")
+
+def creation_pion(x, y, r, coul):
+	"Trace un pion"
+	can.create_oval(x-r, y-r, x+r, y+r, fill = coul)
+
 taille_carre = 40 # permet de définir une taille de damier modifiable
 fen = Tk()
 can = Canvas(fen, width = taille_carre * 8, height = taille_carre * 8, bg = "white")
 can.pack(side = TOP, padx = 5, pady = 5)
+can.bind("<Button-1>", pion)
 b1 = Button(fen, text = "damier", command = damier)
 b1.pack(side = LEFT, padx = 5, pady = 5) 
+b2 = Button(fen, text = "Pion", command = pion)
+b2.pack(side = RIGHT, padx = 5, pady = 5)
 fen.mainloop()
